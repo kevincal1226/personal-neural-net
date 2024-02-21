d71af0e057692c8ca2107ebb203dbf90b29d0c0b
kevincal1226 <kevincal@umich.edu>
2024-02-20 21:02:24 -0500

    ok

--- network.py
+++ network.py
@@ -2,6 +2,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+
+
 def main():
     arrSize = 10
     neuronOff = np.empty([arrSize, arrSize])
@@ -14,14 +16,6 @@ def main():
     plt.scatter(onX, onY, color='b')
     plt.title("Completely Off Neuron Grid")
     plt.show()
-    """
-    l1: list = [i for i in range(10)]
-    print(l1)
-    l2: list = [i for i in range(10, 20)]
-    print(l2)
-    for i, (x, y) in enumerate(zip(l1, l2)):
-        print(f"At index {i}, L1 is {x} and L2 is {y}")
-    """
 
 if __name__ == "__main__":
     main()
